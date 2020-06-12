6f8bbb53fef9d719fa75945a9a98cd26f8c77521
applejenny66 <applejenny66@gmail.com>
2020-06-12 16:45:14 +0800

    .

--- img_proc.py
+++ img_proc.py
@@ -6,7 +6,8 @@ from adafruit_motorkit import MotorKit
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 from PIL import Image
-
+import cv2
+import numpy as np
 from car import car
 
 class image_procseeing():
@@ -15,7 +16,31 @@ class image_procseeing():
         #(1080, 1920, 3)
 
     def find_line(self):
-        pass
+        self.gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+        kernel_size = 5
+        self.blur_gray = cv2.GaussianBlur(gray, (kernel_size, kernel_size), 0)
+        
+        low_thres = 50
+        high_thres = 150
+        edges = cv2.Canny(self.blur_gray, low_thres, hogh_thres)
+        
+        rho = 1
+        theta = np.pi / 180
+        threshold = 15
+        min_line_length = 50
+        max_line_gap = 20
+        
+        line_image = np.copy(self.img) * 0
+        
+        lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),\
+                min_line_length, max_line_gap)
+
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 5)
+
+        lines_edges = cv2.addWeighted(self.img, 0.8, line_image, 1, 0)
+        cv2.imwrite("line.png", lines_edges)
 
 
 if __name__ == "__main__":
@@ -30,3 +55,4 @@ if __name__ == "__main__":
     #print ("img type: ", type(image))
     procs = image_procseeing(resize_im)
     print ("shape: ", procs.img.size)
+    procs.find_line()
