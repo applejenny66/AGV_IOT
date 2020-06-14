723003a5b29184a86cb011ec9353a56a2a49f9e8
applejenny66 <applejenny66@gmail.com>
2020-06-14 21:52:12 +0800

    .

--- front_gear.py
+++ front_gear.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-
+import time
 CONTROL_PIN = 17
 PWM_FREQ = 50
 STEP = 15
@@ -12,8 +12,14 @@ pwm = GPIO.PWM(CONTROL_PIN, PWM_FREQ)
 pwm.start(0)
 
 def angle_to_duty_cycle(angle=0):
-    duty_cycle = (0.05 * PWM_FREQ) + (0.19 * PWM_FREQ * angle / 180)
-    return (duty_cycle)
-angle =  50
-dc = angle_to_duty_cycle(angle)
-pwm.ChangeDutyCycle(dc)
+    if 50<=angle<=110:
+        GPIO.output(CONTROL_PIN, True)
+        dc = (0.05 * PWM_FREQ) + (0.19 * PWM_FREQ * angle / 180)
+        pwm.ChangeDutyCycle(dc)
+        time.sleep(1)
+        GPIO.output(CONTROL_PIN, False)
+def turn_left():
+    angle_to_duty_cycle(50)
+def turn_right():
+    angle_to_duty_cycle(110)
+turn_left()
